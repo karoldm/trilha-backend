d247a79750878e851ff6db6a57db8ba33c1e656d
karol marques <karoldm1579@gmail.com>
2021-07-16 16:44:50 -0300

    Procfile

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes');
 const cors = require('cors');
+const routes = require('./routes');
 
 require('dotenv').config();
 require('./database');
 
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
@@ -16,5 +15,5 @@ app.use('/images', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log('rodando na porta', port);
+    console.log('rodando na porta ' + port);
 });
